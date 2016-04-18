4e050e10602f0a2d000f8b2431a0b9cb693cc1f8
Denis Gordin <gordin.dan@gmail.com>
2016-04-18 20:58:16 +0300

    add email interface

---
+++ samples/MvvmCross.Plugins/MvvmCross.Plugins/Plugins/ISimpleEmailPlugin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace g0rdan.MvvmCross.Plugins
+{
+    public interface ISimpleEmailPlugin
+    {
+        void SendEmail(string toEmail, string subject, string message);
+    }
+}
+
