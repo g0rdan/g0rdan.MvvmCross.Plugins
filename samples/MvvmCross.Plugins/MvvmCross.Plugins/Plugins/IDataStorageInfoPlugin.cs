3759dc87a0b3a9837eef1884433fab75086028c4
Denis Gordin <gordin.dan@gmail.com>
2016-05-14 20:51:07 +0300

    #

--- samples/MvvmCross.Plugins/MvvmCross.Plugins/Plugins/IDataStorageInfoPlugin.cs
+++ samples/MvvmCross.Plugins/MvvmCross.Plugins/Plugins/IDataStorageInfoPlugin.cs
@@ -3,8 +3,8 @@ namespace g0rdan.MvvmCross.Plugins
 {
     public interface IDataStorageInfoPlugin
     {
-        decimal GetTotalSpace (MemorySizeType mSizeType);
-        decimal GetFreeSpace (MemorySizeType mSizeType);
+        decimal GetTotalSpace (MemorySizeType mSizeType = MemorySizeType.Bytes);
+        decimal GetFreeSpace (MemorySizeType mSizeType = MemorySizeType.Bytes);
     }
 }
 
