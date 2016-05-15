c334e86cf4bcee5448396334d5088845ec4e2305
Denis Gordin <gordin.dan@gmail.com>
2016-05-15 10:19:53 +0300

    droid add new parametr

--- samples/MvvmCross.Plugins/Droid/Plugins/DataStorageInfoPlugin.cs
+++ samples/MvvmCross.Plugins/Droid/Plugins/DataStorageInfoPlugin.cs
@@ -1,4 +1,5 @@
 ﻿//using System;
+using System;
 using Android.OS;
 using Java.IO;
 
@@ -11,34 +12,42 @@ namespace g0rdan.MvvmCross.Plugins.Droid
 
         public DataStorageInfoPlugin ()
         {
-            File path = Environment.DataDirectory;
+            File path = Android.OS.Environment.DataDirectory;
             StatFs stat = new StatFs (path.Path);
             _freeSpace = stat.BlockSizeLong;
             _totalSpace = stat.AvailableBlocksLong;
         }
 
-        public decimal GetFreeSpace (MemorySizeType mSizeType = MemorySizeType.Bytes)
+        public decimal GetFreeSpace (MemorySizeType mSizeType = MemorySizeType.Bytes, int digits = 2)
         {
-            return ConvertTo (_freeSpace, mSizeType);
+            return ConvertTo (_freeSpace, mSizeType, digits);
         }
 
-        public decimal GetTotalSpace (MemorySizeType mSizeType = MemorySizeType.Bytes)
+        public decimal GetTotalSpace (MemorySizeType mSizeType = MemorySizeType.Bytes, int digits = 2)
         {
-            return ConvertTo (_totalSpace, mSizeType);
+            return ConvertTo (_totalSpace, mSizeType, digits);
         }
 
-        private decimal ConvertTo (long bytes, MemorySizeType mSizeType = MemorySizeType.Bytes)
+        private decimal ConvertTo (long bytes, MemorySizeType mSizeType, int digits)
         {
+            decimal result;
+            decimal divider = 1024;
+
             switch (mSizeType) {
             case MemorySizeType.KBytes:
-                return bytes / 1024;
+                result = bytes / divider;
+                break;
             case MemorySizeType.MBytes:
-                return bytes / 1024 / 1024;
+                result = bytes / divider / divider;
+                break;
             case MemorySizeType.GBytes:
-                return bytes / 1024 / 1024 / 1024;
+                result = bytes / divider / divider / divider;
+                break;
             default:
                 return bytes;
             }
+
+            return Math.Round (result, digits);
         }
     }
 }
