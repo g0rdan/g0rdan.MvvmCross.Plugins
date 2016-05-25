4c8f116e609db95106cc09ecd74a047ec5cb37b5
Denis Gordin <gordin.dan@gmail.com>
2016-05-25 21:28:47 +0300

    windows: add empty disk info plugin

---
+++ source/Windows/DiskInfo/g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon/DiskInfoPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon
+{
+    public class DiskInfoPlugin : IDiskInfoPlugin
+    {
+        public decimal GetFreeSpace(DeviceType deviceType = DeviceType.Inner, MemorySizeType mSizeType = MemorySizeType.Bytes, int digits = 2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetTotalSpace(DeviceType deviceType = DeviceType.Inner, MemorySizeType mSizeType = MemorySizeType.Bytes, int digits = 2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/Windows/DiskInfo/g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.Platform;
+using MvvmCross.Platform.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon
+{
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterSingleton<IDiskInfoPlugin>(new DiskInfoPlugin());
+        }
+    }
+}
