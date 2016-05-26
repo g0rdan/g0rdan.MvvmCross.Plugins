2a5edf483f5cd93d29cc9a9a237c9f267ca86a28
Denis Gordin <gordin.dan@gmail.com>
2016-05-26 11:53:24 +0300

    windows: add get free space implementation

--- source/Windows/DiskInfo/g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon/DiskInfoPlugin.cs
+++ source/Windows/DiskInfo/g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon/DiskInfoPlugin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon
 {
@@ -10,7 +11,10 @@ namespace g0rdan.MvvmCross.Plugin.DiskInfo.WindowsCommon
     {
         public decimal GetFreeSpace(DeviceType deviceType = DeviceType.Inner, MemorySizeType mSizeType = MemorySizeType.Bytes, int digits = 2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            StorageFolder local = ApplicationData.Current.LocalFolder;
+            var retrivedProperties = local.Properties.RetrievePropertiesAsync(new string[] { "System.FreeSpace" }).GetResults();
+            return (decimal)retrivedProperties["System.FreeSpace"];
         }
 
         public decimal GetTotalSpace(DeviceType deviceType = DeviceType.Inner, MemorySizeType mSizeType = MemorySizeType.Bytes, int digits = 2)
