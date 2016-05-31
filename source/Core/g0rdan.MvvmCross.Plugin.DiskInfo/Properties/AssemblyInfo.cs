e1950eb50820a3a52631ef8660161b8706c2b6d6
Denis Gordin <gordin.dan@gmail.com>
2016-05-31 20:57:34 +0300

    disk info plugin: add nuget spec and change some assembly info

--- source/Core/g0rdan.MvvmCross.Plugin.DiskInfo/Properties/AssemblyInfo.cs
+++ source/Core/g0rdan.MvvmCross.Plugin.DiskInfo/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("g0rdan.MvvmCross.Plugin.DiskInfo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MvvmCross DiskInfo Plugin")]
+[assembly: AssemblyDescription("MvvmCross plugin for getting storage data info")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("g0rdan")]
+[assembly: AssemblyCopyright("Denis Gordin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
